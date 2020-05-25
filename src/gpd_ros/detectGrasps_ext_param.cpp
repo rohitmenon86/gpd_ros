994a61a8051d055bf9ef6221710cfc3f41fa6284
Atmaraaj Gopal <atmaraaj.gopal@hansrobot.de>
2020-05-25 09:04:29 +0200

    demo ready version

--- src/gpd_ros/detectGrasps_ext_param.cpp
+++ src/gpd_ros/detectGrasps_ext_param.cpp
@@ -39,13 +39,12 @@ std::string config_file;
 
 std::string frame_; ///< point cloud frame
 bool has_cloud = false;
+bool cb_run = false; // prevent run in cb before initialization
 
 gpd::util::Cloud *cloudPtr;
 gpd::GraspDetector* detector;
 GraspPlotter* rviz_plotter_;
 std::vector<std::unique_ptr<gpd::candidate::Hand>> grasps;
-//ros::Rate* loop_rate;
-
 
 void getPointCloud()
 {
@@ -56,12 +55,22 @@ void getPointCloud()
     
     
     ros::Time request_time = ros::Time::now();
-    ros::Time point_cloud_time = request_time - ros::Duration(0.1);
+    ros::Time point_cloud_time;
+    
+    // Exception handling, negative time when running simulation
+    try {
+        point_cloud_time = request_time - ros::Duration(0.1);
+    } catch (std::runtime_error& ex) {
+        ROS_ERROR("Exception: [%s]", ex.what());
+        point_cloud_time = request_time;
+        request_time = request_time + ros::Duration(0.1);
+    }
+
     ROS_INFO("Awaiting point cloud.");
     while (point_cloud_time < request_time)
     { 
         sensor_msgs::PointCloud2ConstPtr pc_msg = 
-        ros::topic::waitForMessage<sensor_msgs::PointCloud2>("camera/depth/points", ros::Duration(10.0));
+        ros::topic::waitForMessage<sensor_msgs::PointCloud2>("camera/depth/points", ros::Duration(5.0));
 
         // get the latest point cloud
         if (pc_msg == NULL)
@@ -79,8 +88,6 @@ void getPointCloud()
             pcl_conversions::toPCL(*pc_msg, pcl_pc2);
             pcl::fromPCLPointCloud2(pcl_pc2, *pc);
 
-            Eigen::Matrix3Xd view_points(3, 1);
-            view_points = myParam.camera_position;
             cloudPtr = new gpd::util::Cloud(pc, 0, myParam.camera_position);
             ROS_INFO_STREAM("Received cloud with " << cloudPtr->getCloudProcessed()->size() << " points.");
             has_cloud = true;
@@ -90,8 +97,6 @@ void getPointCloud()
         point_cloud_time = pc_msg->header.stamp;
         frame_ = pc_msg->header.frame_id;
     }
-    
-    
 }
 
 void run()
@@ -117,11 +122,15 @@ void run()
                 {
                     ROS_INFO("Preprocessing cloud.");
                     // detect grasps
-                    detector->preprocessPointCloud(*cloudPtr);
+                    detector->preprocessPointCloud(*cloudPtr, myParam.workspace);
                     ROS_INFO("Detecting grasps.");
                     grasps = detector->detectGrasps(*cloudPtr, myParam);
                     ROS_INFO("Detected!");
-                
+
+                    std::cout << "New point cloud?"
+                    "Press y for new cloud, any other key for same point cloud" << std::endl;
+                    std::cin >> input;
+                    if (input == 'y') {has_cloud = false;}
                 }
             }
             else if (input == 'n')
@@ -131,20 +140,19 @@ void run()
             }
             else
             {
-                //loop_rate->sleep();
                 continue;
             }
         } else{
             ROS_INFO("Getting new point_cloud...");
             getPointCloud();
         }
-        //loop_rate->sleep();
     }
 }
 
 //dynamic configCallback
 void configCallback(gpd_ros::detect_graspsConfig &config, uint32_t level)
 {
+    ROS_WARN("CALLBACK CALLED");
     myParam.camera_position.resize(3,1);
     myParam.workspace.resize(6);
     // modify params to type required
@@ -156,12 +164,12 @@ void configCallback(gpd_ros::detect_graspsConfig &config, uint32_t level)
                                config.groups.camera_position.y1, 
                                config.groups.camera_position.z1;
 
-    myParam.workspace.assign({config.groups.workspace.i0, 
-                            config.groups.workspace.i1, 
-                            config.groups.workspace.i2, 
-                            config.groups.workspace.i3, 
-                            config.groups.workspace.i4, 
-                            config.groups.workspace.i5});
+    myParam.workspace.assign({config.groups.workspace.size*-1, 
+                            config.groups.workspace.size*1, 
+                            config.groups.workspace.size*-1, 
+                            config.groups.workspace.size*1, 
+                            config.groups.workspace.size*-1, 
+                            config.groups.workspace.size*1});
 
     myParam.thresh_rad = config.thresh_rad;
     myParam.approach_direction = config.approach_direction;
@@ -178,8 +186,6 @@ void configCallback(gpd_ros::detect_graspsConfig &config, uint32_t level)
             getPointCloud();
         }
     }
-
-    run();
 }
 
 
@@ -188,18 +194,14 @@ int main(int argc, char **argv)
     // Set up ROS.
     ros::init(argc, argv, "detect_grasp_test");
     ros::NodeHandle nh;
-    //loop_rate = new ros::Rate(1);
-
     ros::Publisher grasps_rviz_pub_ = nh.advertise<visualization_msgs::MarkerArray>("plot_grasps", 1);
-    
     
     //! Dynamic reconfigure server.
     dynamic_reconfigure::Server<gpd_ros::detect_graspsConfig> dr_srv_;
     dynamic_reconfigure::Server<gpd_ros::detect_graspsConfig>::CallbackType cb;
     cb = boost::bind(&configCallback, _1, _2);
     dr_srv_.setCallback(cb);
-
-    
+   
     {
         nh.getParam("camera_position", camera_position);
         nh.getParam("workspace", workspace);
@@ -211,24 +213,23 @@ int main(int argc, char **argv)
         nh.param<std::string>("config_file", config_file, "/home/atmaraaj/hrg/gpd/cfg/eigen_params.cfg");
 
         // Initialize param values, modify params to type required
-        myParam.camera_position.resize(3,1);
+        
         myParam.workspace.resize(6);
+        myParam.camera_position.resize(3,1);
 
-        myParam.direction           << direction[0], direction[1], direction[2];
-        myParam.camera_position     << camera_position[0], camera_position[1], camera_position[2];
+        myParam.direction       << direction[0], direction[1], direction[2];
+        myParam.camera_position << camera_position[0], camera_position[1], camera_position[2];
         myParam.workspace           = workspace;
         myParam.thresh_rad          = thresh_rad;
         myParam.approach_direction  = approach_direction; 
     }
     
-
     detector = new gpd::GraspDetector(config_file);
     //rviz_plotter_ = new GraspPlotter(nh, detector->getHandSearchParameters().hand_geometry_);
 
-    getPointCloud();
-
     // run detection
     run();
+
     //rviz_plotter_->drawGrasps(grasps, frame_);
 
     // Let ROS handle all callbacks.
